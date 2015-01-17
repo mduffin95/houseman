b373be4bcb9e2c3ba05055bb48d60ea89d4464ba
Matthew Duffin <matthewjduffin@gmail.com>
2015-01-17 18:48:03 +0000

    floor numbers in place

--- models.py
+++ models.py
@@ -3,10 +3,17 @@ from django.db import models
 
 class Floor(models.Model):
     name = models.CharField(max_length=200)
+    number = models.IntegerField(unique=True)
+    
+    def __str__(self):
+        return self.name
     
 class Room(models.Model):
     name = models.CharField(max_length=200)
     floor = models.ForeignKey(Floor)
+    
+    def __str__(self):
+        return self.name
     
 class Appliance(models.Model):
     name = models.CharField(max_length=200)
@@ -14,7 +21,7 @@ class Appliance(models.Model):
     create_date = models.DateTimeField('date created')
     room = models.ForeignKey(Room)
     
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):
         return self.name
         
     def on(self):
