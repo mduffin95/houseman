1f68988e44d4ce58201c0621f5b7f1c30aa3f84f
Matthew Duffin <matthewjduffin@gmail.com>
2015-01-17 23:23:42 +0000

    missed an import, adding now

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from houseman.models import Appliance
 from django.shortcuts import render, get_object_or_404
 
