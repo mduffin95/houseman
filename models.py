3529d5b6b9edb2cb7fe6153839435cd92b0aeff3
Matthew Duffin <matthewjduffin@gmail.com>
2015-01-17 18:54:30 +0000

    Had to add a default to allow uniqueness

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Floor(models.Model):
     name = models.CharField(max_length=200)
-    number = models.IntegerField(unique=True)
+    number = models.IntegerField(unique=True, default=0)
     
     def __str__(self):
         return self.name
