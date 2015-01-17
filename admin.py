ad5c0c7aa764da4f3b056a63af5a3ebb35822bab
Matthew Duffin <matthewjduffin@gmail.com>
2015-01-17 17:30:41 +0000

    inline functionality added. Admin should now work

--- admin.py
+++ admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from houseman.models import Appliance
+from houseman.models import Appliance, Room, Floor
 
-# Register your models here.
-admin.site.register(Appliance)
+class ChoiceInLine(admin.StackedInLine):
+    model = Appliance
+    extra = 3
+
+class RoomAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        (None, {'fields': ['floor']}),
+    ]
+    inlines = [ChoiceInLine]
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Floor)
+
+
