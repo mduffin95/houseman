d3238489200be3a98ae3afb0e99bae705ca46823
Matthew Duffin <matthewjduffin@gmail.com>
2015-01-18 00:10:03 +0000

    bug fix in models.py on and off functions in Appliance model

--- models.py
+++ models.py
@@ -25,10 +25,10 @@ class Appliance(models.Model):
         return self.name
         
     def on(self):
-        call(["irsend","SEND_START",lirc_dev,"on"])
+        call(["irsend","SEND_START",self.lirc_dev,"on"])
         
     def off(self):
-        call(["irsend","SEND_START",lirc_dev,"off"])
+        call(["irsend","SEND_START",self.lirc_dev,"off"])
     
     #Could add this later to allow different types of switch, eg. dimmers.
     #class Meta:
