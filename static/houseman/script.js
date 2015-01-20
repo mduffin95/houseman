656286aea7434118616d8435b537a94bda120d25
Matthew Duffin <matthewjduffin@gmail.com>
2015-01-20 02:53:50 +0000

    csrf token added to script

---
+++ static/houseman/script.js
@@ -0,0 +1,42 @@
+// https://docs.djangoproject.com/en/1.7/ref/contrib/csrf/
+// https://gist.github.com/rca/1696408
+// using jQuery
+function getCookie(name)
+{
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+ 
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+ 
+$.ajaxSetup({ 
+     beforeSend: function(xhr, settings) {
+         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+             // Only send the token to relative URLs i.e. locally.
+             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+         }
+     } 
+});
+
+$("input").change(
+    function() {
+        var dataString =  'state=' + $( this ).attr( "checked" );
+        var urlWithId = "/" + $(this).attr('name') + "/process/";
+        $.ajax
+        ({
+            type: "POST",
+            url: urlWithId,
+            data: dataString,
+        });
+    }
+)
