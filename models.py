3ead13c89a7116b1031f186342feee187c959c9c
Matthew Duffin <matthewjduffin@gmail.com>
2015-01-20 01:40:26 +0000

    fixed boolean default

--- models.py
+++ models.py
@@ -20,7 +20,7 @@ class Appliance(models.Model):
     lirc_dev = models.CharField('lirc device name', max_length=200)
     create_date = models.DateTimeField('date created')
     room = models.ForeignKey(Room)
-    state = models.BooleanField(initial=False)
+    state = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
