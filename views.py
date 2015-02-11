5c731d2d3d59c08c6e11bf10076f9fe1ba39ba56
Matthew Duffin <matthewjduffin@gmail.com>
2015-02-11 22:58:23 +0000

    Updated views to use InheritanceManager

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from subprocess import CalledProcessError
 from time import sleep
+from model_utils.managers import InheritanceManager
 
 class IndexView(generic.ListView):
     model = Appliance
@@ -17,7 +18,7 @@ class IndexView(generic.ListView):
 def process(request, appliance_id):
     try:
         stateStr = request.POST['state']    
-        app = Appliance.objects.get(pk=appliance_id)
+        app = Appliance.objects.get_subclass(pk=appliance_id) #This ensures a Curtain will be returned instead of an Appliance.
     except (Appliance.DoesNotExist, KeyError):
         return HttpResponseBadRequest("Something went wrong. stateStr=" + stateStr)
     
