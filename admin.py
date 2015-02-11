d1d3f91e1f7de56e822859eeb21955789e6e2f2e
Matthew Duffin <matthewjduffin@gmail.com>
2015-02-11 23:12:14 +0000

    Created a CurtainInLine so that curtains can be added

--- admin.py
+++ admin.py
@@ -4,13 +4,16 @@ from houseman.models import Appliance, Room, Floor
 class ApplianceInline(admin.StackedInline):
     model = Appliance
     extra = 3
+    
+class CurtainInline(admin.StackedInline):
+    model = Curtain
+    extra = 1
 
 class RoomAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['name']}),
-        (None, {'fields': ['floor']}),
-    ]
-    inlines = [ApplianceInline]
+    inlines = [
+        ApplianceInline,
+        CurtainInLine,
+        ]
 
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Floor)
